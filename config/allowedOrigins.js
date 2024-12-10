2560246d137c422e7006c7d657cf4e052db2de82
reihiraga <royhan.h.2801@gmail.com>
2024-12-10 12:58:48 +0700

    add

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,8 @@
 // whitelist --> url yang dapat mengakses/me-request ke backend
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "https://reparasi-client-opk1.onrender.com",
+];
 
 module.exports = allowedOrigins;
