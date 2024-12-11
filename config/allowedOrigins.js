3805168d35a952600598d4342dcf9083d2d65dc0
reihiraga <royhan.h.2801@gmail.com>
2024-12-11 09:12:24 +0700

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,4 @@
 // whitelist --> url yang dapat mengakses/me-request ke backend
-// whitelist --> url yang dapat mengakses/me-request ke backend
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
